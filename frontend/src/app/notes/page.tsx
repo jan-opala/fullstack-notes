e64f67e94d41807b2c1aed7c99da73f4deade42b
Jan Opala <jan.opala@yahoo.com>
2025-05-25 22:39:42 +0200

    Add a todo list. Resize the sidebar. Rename note to currentNote for better code. Add syncing with server every 1 second. Change the delay between the input and uploading data from 500ms to 1250ms. Add some comments. Add remove note functionality and button. Add create note functionality and button.

--- frontend/src/app/notes/page.tsx
+++ frontend/src/app/notes/page.tsx
@@ -6,12 +6,18 @@ import { useNotes } from "../hooks/useNotes";
 import { redirect } from 'next/navigation';
 import Image from 'next/image';
 
+// TODO:
+// Markdown implementation (edit view and markdown view OR combined)
+// When creating a note open it as default
+// Make it beautiful
+// After choosing note on mobile, close the sidebar.
+
 function SidebarButton({ opened, setOpened }: { opened: boolean; setOpened: React.Dispatch<React.SetStateAction<boolean>> }) {
   if (opened == true){
     return (
       <button onClick={() => {
         setOpened(false);
-      }} className="absolute top-1/2 left-[164px] transform -translate-y-1/2 bg-stone-700 text-black p-2 rounded-l-md shadow-md hover:bg-stone-600">
+      }} className="absolute top-1/2 left-[266px] transform -translate-y-1/2 bg-stone-700 text-black p-2 rounded-l-md shadow-md hover:bg-stone-600">
         <svg
           className="w-5 h-5"
           fill="none"
@@ -58,27 +64,43 @@ export default function Notes() {
   const [ opened, setOpened ] = useState(false);
   const { isAuthenticated } = useAuth();
   const { notes, refreshNotes } = useNotes(isAuthenticated);
-  const [note, setNote] = useState<Note | null>(null);
+  const [currentNote, setCurrentNote] = useState<Note | null>(null);
   const [content, setContent] = useState<string | null>(null);
   const [username, setUsername] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [uploading, setUploading] = useState<boolean>(false);
   const [renaming, setRenaming] = useState<boolean>(false);
+  const [removing, setRemoving] = useState<boolean>(false);
 
+  // fetch username from cookie
   useEffect(() => {
     const match = document.cookie.match(/(?:^|; )username=([^;]*)/);
     setUsername(match ? decodeURIComponent(match[1]) : null);
   }, []);
 
-
-
+  // Handle current note refresh when notes refresh.
   useEffect(() => {
-    if (!note || content === note.content) return;
+    if (currentNote) {
+      
+      const updatedNote = notes?.find((note) => note.id === currentNote.id);
+      if (updatedNote) {
+        setCurrentNote(updatedNote);
+        setContent(updatedNote.content);
+      } else {
+        setCurrentNote(null);
+        setContent(null);
+      }
+  }
+  }, [notes]);
+
+  // Upload content of note after user stops typing for 1250ms
+  useEffect(() => {
+    if (!currentNote || content === currentNote.content) return;
     setUploading(true);
     const timeout = setTimeout(async () => {
       try {
         const res = await axios.post("http://localhost:8000/api/update-note", {
-          id: note.id,
+          id: currentNote.id,
           content: content
         }, { withCredentials: true });
         if (res.status !== 200) {
@@ -90,14 +112,26 @@ export default function Notes() {
         setTimeout(() => {setError(null)}, 15000);
       }
       setUploading(false);
-    }, 1250); // 500ms debounce
+    }, 1250);
 
     return () => {
       setUploading(false);
       clearTimeout(timeout);
     };
-  }, [content, note]);
+  }, [content, currentNote]);
 
+  // Sync local notes with server every 1 second.
+  useEffect(() => {
+    if (!uploading) {
+      const interval = setInterval(() => {
+        refreshNotes();
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [uploading]);
+
+  // Check if user is authenticated, if not redirect him to login page.
   if (isAuthenticated === null) return(
       <div className="flex items-center justify-center h-screen bg-black">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-stone-500"></div>
@@ -110,8 +144,8 @@ export default function Notes() {
   }
 
 const changeNote = async (newNote: Note | null) => {
-  if (note != null && note.content != content) {
-    const _note = note;
+  if (currentNote != null && currentNote.content != content) {
+    const _note = currentNote;
     _note.content = content ?? "";
     try {
       const res = await axios.post("http://localhost:8000/api/update-note", {
@@ -130,12 +164,12 @@ const changeNote = async (newNote: Note | null) => {
   }
 
   if (newNote === null) {
-    setNote(null);
+    setCurrentNote(null);
     setContent("");
     return;
   }
 
-  setNote(newNote);
+  setCurrentNote(newNote);
   setContent(newNote.content ?? "");
 }
 
@@ -159,6 +193,43 @@ const renameNote = async (note: Note | null, newTitle: string | null) => {
   }
 }
 
+const removeNote = async (note: Note | null) => {
+  if (note != null) {
+    try {
+      const res = await axios.post("http://localhost:8000/api/delete-note", {
+        "id": note.id,
+      }, {withCredentials: true,});
+      if (res.status !== 200) {
+        setError("Failed remove the note.");
+        setTimeout(() => {setError(null)}, 15000);
+      }
+      if (note == currentNote) {
+        setCurrentNote(null);
+      }
+    } catch (error) {
+      setError("Failed to remove the note.");
+      setTimeout(() => {setError(null)}, 15000);
+    }
+  }
+}
+
+const createNote = async () => {
+  try {
+    const res = await axios.post("http://localhost:8000/api/new-note", {
+      "title": "New note"
+    }, {withCredentials: true,});
+    if (res.status !== 200) {
+      setError("Failed to create a new note.");
+      setTimeout(() => {setError(null)}, 15000);
+    }
+    refreshNotes();
+  } catch (error) {
+    setError("Failed to create a new note.");
+    setTimeout(() => {setError(null)}, 15000);
+  }
+}
+
+
 function NotesSkeleton() {
   return (
     <div>
@@ -173,22 +244,19 @@ function NotesSkeleton() {
   );
 }
 
-const listNotes = notes == null ? (
-  <NotesSkeleton />
-) : (
-  notes.map(nnote => (
-    <div
-      onClick={() => { changeNote(nnote); }}
-      key={nnote.id}
-      className={nnote.id === note?.id 
-        ? "text-stone-200 py-1 pl-1 border-b border-stone-600 border-solid bg-stone-800 hover:bg-stone-800 cursor-pointer"
-        : "text-stone-200 py-1 pl-1 border-b border-stone-600 border-solid hover:bg-stone-800 cursor-pointer"
-      }
-    >
-      {nnote.title}
-    </div>
-  ))
-);
+const listNotes = notes == null ? (<NotesSkeleton />) : (
+    notes.map(nnote => (
+      <div
+        onClick={() => { changeNote(nnote); }}
+        key={nnote.id}
+        className={nnote.id === currentNote?.id 
+          ? "text-stone-200 py-1 pl-1 border-b border-stone-600 border-solid bg-stone-800 hover:bg-stone-800 cursor-pointer"
+          : "text-stone-200 py-1 pl-1 border-b border-stone-600 border-solid hover:bg-stone-800 cursor-pointer"
+        }
+      >
+        {nnote.title}
+      </div>
+    )));
 
   return (
     <div className="min-h-screen h-screen overflow-hidden font-sans">
@@ -268,7 +336,7 @@ const listNotes = notes == null ? (
                     type="text"
                     id="newtitle"
                     className="mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
-                    defaultValue={note?.title}
+                    defaultValue={currentNote?.title}
                     ref={newTitleRef}
                   />
                 </label>
@@ -289,11 +357,18 @@ const listNotes = notes == null ? (
                   const newTitle = newTitleRef.current?.value;
                   if (newTitle == "" || newTitle == null || newTitle == undefined) {
                     setError("Title of the note cannot be empty.");
-                    setTimeout(() => {setError(null);}, 2000)
+                    setTimeout(() => {setError(null);}, 3000)
+                    setRenaming(false);
+                    return;
+                  }
+                  if (newTitle.length > 50) {
+                    setError("Title of the note cannot be longer than 50 characters.")
+                    setTimeout(() => {setError(null);}, 3000)
+                    setRenaming(false);
                     return;
                   }
                   setRenaming(false);
-                  renameNote(note, newTitle);
+                  renameNote(currentNote, newTitle);
                   
                 }}
                   type="button"
@@ -308,23 +383,96 @@ const listNotes = notes == null ? (
       </>
       }
 
-      {/* TOP BAR */}
-      <div className="flex flex-wrap items-center justify-between mx-auto bg-stone-900 pl-2 pr-2 text-white h-[35px] border-white border-b">
-        <div className="font-bold flex text-xl">
-          <div className="mx-1">+</div>
-          <div className="mx-1">-</div>
+      {/* Note removing */}
+      {removing && 
+      <>
+        <div className="text-white absolute w-full h-full bg-transparent text-center items-center backdrop-blur-[2px]">
+          <div className="fixed inset-0 z-50 grid place-content-center bg-black/50 p-4"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modalTitle"
+            >
+            <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900">
+              <div className="flex items-start justify-between">
+                <h2 id="modalTitle" className="text-xl font-bold text-gray-900 sm:text-2xl dark:text-white">
+                  Remove note
+                </h2>
+
+                <button
+                  onClick={() => {setRemoving(false);}}
+                  type="button"
+                  className="-me-4 -mt-4 rounded-full p-2 text-gray-400 transition-colors hover:bg-gray-50 hover:text-gray-600 focus:outline-none dark:text-gray-500 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+                  aria-label="Close"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div className="mt-4">
+                <p className="text-pretty text-gray-700 dark:text-gray-200">
+                  Are you sure you want to remove this note?
+                </p>
+              </div>
+
+              <footer className="mt-6 flex justify-end gap-2">
+                <button
+                  onClick={() => {setRemoving(false);}}
+                  type="button"
+                  className="rounded bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700"
+                >
+                  No, cancel.
+                </button>
+
+                <button
+                onClick={() => {
+                  
+                  setRemoving(false);
+                  removeNote(currentNote);
+                  
+                }}
+                  type="button"
+                  className="rounded bg-red-700 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-blue-700"
+                >
+                  Yes.
+                </button>
+              </footer>
+            </div>
+          </div>
         </div>
-        {note == null ? (
+      </>
+      }
+
+
+      {/* TOP BAR */}
+      <div className="flex flex-wrap items-center justify-between mx-auto bg-stone-900 pl-2 pr-2 text-white h-[50px] border-white border-b">
+        <div className="font-bold flex text-xl items-center">
+          <div onClick={createNote} className="mx-1 cursor-pointer"><Image alt="" src="svg/create.svg" width={20} height={20} /></div>
+        </div>
+        {currentNote == null ? (
           <div className="font-bold">NOTES</div>
         ) : (
           <div className="flex items-center">
-            <div onClick={() => {changeNote(null)}} className="cursor-pointer mr-1">
-              <Image alt="" src="svg/x.svg" width={20} height={20} />
+            <div onClick={() => {changeNote(null)}} className="cursor-pointer mr-2">
+              <Image alt="" src="svg/x.svg" width={22} height={22} />
             </div>
-            <div className="font-bold">{note.title}</div>
-            <div onClick={() => {setRenaming(true)}} className="ml-2 fill-white cursor-pointer"><Image alt="" src="svg/rename.svg" width={20} height={20} /></div>
-            { uploading ? <div className="ml-2"><Image className="animate-bounce" alt="" src="svg/cloud.svg" width={22} height={22}/></div>
-                        : <div className="ml-2"><Image className="invisible" alt="" src="svg/cloud.svg" width={22} height={22}/></div>}
+            <div className="font-bold">{currentNote.title}</div>
+            <div onClick={() => {setRenaming(true)}} className="ml-2 fill-white cursor-pointer"><Image alt="" src="svg/rename.svg" width={22} height={22} /></div>
+            <div onClick={() => {setRemoving(true)}} className="ml-2 fill-white cursor-pointer"><Image alt="" src="svg/delete.svg" width={22} height={22} /></div>
+            { uploading ? <div className="ml-2"><Image className="animate-bounce" alt="" src="svg/cloud.svg" width={24} height={24}/></div>
+                        : <div className="ml-2"><Image className="invisible" alt="" src="svg/cloud.svg" width={24} height={24}/></div>}
           </div>
         )}
         
@@ -333,15 +481,16 @@ const listNotes = notes == null ? (
           <p>{username}</p>
         </div>
       </div>
-
-      <div className={ opened ? ("h-dvh grid gap-0.5 grid-cols-[200px_1fr] bg-stone-900") : ("h-dvh grid grid-cols-[1px_1fr] gap-0.5 md:grid-cols-[200px_1fr] bg-stone-900") }>
+      
+      {/* NOTE EDITOR AND SIDEBAR (NOTE LIST) */}
+      <div className={ opened ? ("h-dvh grid gap-0.5 grid-cols-[300px_1fr] bg-stone-900") : ("h-dvh grid grid-cols-[1px_1fr] gap-0.5 md:grid-cols-[300px_1fr] bg-stone-900") }>
         <div className={ opened ? ("h-full bg-stone-900") : ("h-full bg-stone-900 invisible md:visible") }>
           {listNotes}
         </div>
 
         <div className="h-full bg-stone-800 text-stone-200">
 
-          {note === null ? (
+          {currentNote === null ? (
 
             username == null ? (
               <div className="h-dvh text-center content-center">
