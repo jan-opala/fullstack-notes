72ef298ff6a81fdb4046544ec4538cfb771d9b95
Jan Opala <jan.opala@yahoo.com>
2025-05-25 20:04:08 +0200

    Add note renaming. Show cloud icon when changes are unsaved. Add error messages when syncing fails. Change color of active note on the sidebar.

--- frontend/src/app/hooks/useNotes.ts
+++ frontend/src/app/hooks/useNotes.ts
@@ -10,20 +10,21 @@ type Note = {
 export const useNotes = (isAuthenticated: boolean | null) => {
   const [notes, setNotes] = useState<Note[] | null>(null); // Use the Note type
 
+  const fetchNotes = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/api/my-notes", {
+        withCredentials: true,
+      });
+      setNotes(Array.isArray(res.data) ? res.data : res.data.notes || []); // Ensure res.data matches the Note[] type
+    } catch {
+      setNotes([]);
+    }
+  };
+
   useEffect(() => {
     if (!isAuthenticated) return; // Only fetch if authenticated
-    const fetchNotes = async () => {
-      try {
-        const res = await axios.get("http://localhost:8000/api/my-notes", {
-          withCredentials: true,
-        });
-        setNotes(Array.isArray(res.data) ? res.data : res.data.notes || []); // Ensure res.data matches the Note[] type
-      } catch {
-        setNotes([]);
-      }
-    };
     fetchNotes();
   }, [isAuthenticated]);
 
-  return { notes };
+  return { notes, refreshNotes: fetchNotes };
 };
--- frontend/src/app/notes/page.tsx
+++ frontend/src/app/notes/page.tsx
@@ -1,9 +1,10 @@
 'use client'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from "axios";
 import { useAuth } from "../hooks/checkAuth";
 import { useNotes } from "../hooks/useNotes";
 import { redirect } from 'next/navigation';
+import Image from 'next/image';
 
 function SidebarButton({ opened, setOpened }: { opened: boolean; setOpened: React.Dispatch<React.SetStateAction<boolean>> }) {
   if (opened == true){
@@ -52,13 +53,17 @@ export default function Notes() {
   content: string;
   };
 
+  const newTitleRef = useRef<HTMLInputElement>(null);
+
   const [ opened, setOpened ] = useState(false);
   const { isAuthenticated } = useAuth();
-  const { notes } = useNotes(isAuthenticated);
+  const { notes, refreshNotes } = useNotes(isAuthenticated);
   const [note, setNote] = useState<Note | null>(null);
   const [content, setContent] = useState<string | null>(null);
   const [username, setUsername] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [renaming, setRenaming] = useState<boolean>(false);
 
   useEffect(() => {
     const match = document.cookie.match(/(?:^|; )username=([^;]*)/);
@@ -69,15 +74,28 @@ export default function Notes() {
 
   useEffect(() => {
     if (!note || content === note.content) return;
+    setUploading(true);
+    const timeout = setTimeout(async () => {
+      try {
+        const res = await axios.post("http://localhost:8000/api/update-note", {
+          id: note.id,
+          content: content
+        }, { withCredentials: true });
+        if (res.status !== 200) {
+        setError("Failed to sync the note with the server, be aware of possibility of losing your data!");
+        setTimeout(() => {setError(null)}, 15000);
+      }
+      } catch (error) {
+        setError("Failed to sync the note with the server, be aware of possibility of losing your data!");
+        setTimeout(() => {setError(null)}, 15000);
+      }
+      setUploading(false);
+    }, 1250); // 500ms debounce
 
-    const timeout = setTimeout(() => {
-      axios.post("http://localhost:8000/api/update-note", {
-        id: note.id,
-        content: content
-      }, { withCredentials: true });
-    }, 750); // 500ms debounce
-
-    return () => clearTimeout(timeout);
+    return () => {
+      setUploading(false);
+      clearTimeout(timeout);
+    };
   }, [content, note]);
 
   if (isAuthenticated === null) return(
@@ -95,14 +113,20 @@ const changeNote = async (newNote: Note | null) => {
   if (note != null && note.content != content) {
     const _note = note;
     _note.content = content ?? "";
-    const res = await axios.post("http://localhost:8000/api/update-note", {
-      "id": _note.id,
-      "content": _note.content
-    }, {withCredentials: true,});
-    if (res.status !== 200) {
+    try {
+      const res = await axios.post("http://localhost:8000/api/update-note", {
+        "id": _note.id,
+        "content": _note.content
+      }, {withCredentials: true,});
+      if (res.status !== 200) {
       setError("Failed to sync the note with the server, be aware of possibility of losing your data!");
-      console.error("Failed to update note");
+      setTimeout(() => {setError(null)}, 15000);
     }
+    } catch (error) {
+      setError("Failed to sync the note with the server, be aware of possibility of losing your data!");
+      setTimeout(() => {setError(null)}, 15000);
+    }
+
   }
 
   if (newNote === null) {
@@ -113,6 +137,26 @@ const changeNote = async (newNote: Note | null) => {
 
   setNote(newNote);
   setContent(newNote.content ?? "");
+}
+
+const renameNote = async (note: Note | null, newTitle: string | null) => {
+  if (note != null && newTitle != null) {
+    const _note = note;
+    _note.title = newTitle;
+    try {
+      const res = await axios.post("http://localhost:8000/api/update-note", {
+        "id": _note.id,
+        "title": _note.title
+      }, {withCredentials: true,});
+      if (res.status !== 200) {
+      setError("Failed to change the title of the note.");
+      setTimeout(() => {setError(null)}, 15000);
+    }
+    } catch (error) {
+      setError("Failed to change the title of the note.");
+      setTimeout(() => {setError(null)}, 15000);
+    }
+  }
 }
 
 function NotesSkeleton() {
@@ -132,13 +176,16 @@ function NotesSkeleton() {
 const listNotes = notes == null ? (
   <NotesSkeleton />
 ) : (
-  notes.map(note => (
+  notes.map(nnote => (
     <div
-      onClick={() => { changeNote(note); }}
-      key={note.id}
-      className="text-stone-200 py-1 pl-1 border-b border-stone-600 border-solid hover:bg-stone-800 cursor-pointer"
+      onClick={() => { changeNote(nnote); }}
+      key={nnote.id}
+      className={nnote.id === note?.id 
+        ? "text-stone-200 py-1 pl-1 border-b border-stone-600 border-solid bg-stone-800 hover:bg-stone-800 cursor-pointer"
+        : "text-stone-200 py-1 pl-1 border-b border-stone-600 border-solid hover:bg-stone-800 cursor-pointer"
+      }
     >
-      {note.title}
+      {nnote.title}
     </div>
   ))
 );
@@ -146,9 +193,9 @@ const listNotes = notes == null ? (
   return (
     <div className="min-h-screen h-screen overflow-hidden font-sans">
 
-      {/* alert */}
-      {error != null ? (
-        <div role="alert" className="absolute left-1/2 -translate-x-1/2 max-w-[300px] border-s-4 border-red-700 bg-red-50 p-4">
+      {/* error alert */}
+      {error != null &&
+        <div role="alert" className="absolute top-[50px] left-1/2 -translate-x-1/2 max-w-[300px] border-s-4 border-red-700 bg-red-50 p-4">
           <div className="flex items-center gap-2 text-red-700">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
               <path
@@ -165,13 +212,101 @@ const listNotes = notes == null ? (
             {error}
           </p>
         </div>
-      ) : (<></>)}
-
-
+      }
+      
       {/* Mobile sidebar open button */}
       <div className="absolute h-screen bg-stone-800 md:invisible">
         <SidebarButton opened={opened} setOpened={setOpened} />
       </div>
+
+      {/* Note renaming */}
+      {renaming && 
+      <>
+        <div className="text-white absolute w-full h-full bg-transparent text-center items-center backdrop-blur-[2px]">
+          <div className="fixed inset-0 z-50 grid place-content-center bg-black/50 p-4"
+            role="dialog"
+            aria-modal="true"
+            aria-labelledby="modalTitle"
+            >
+            <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-gray-900">
+              <div className="flex items-start justify-between">
+                <h2 id="modalTitle" className="text-xl font-bold text-gray-900 sm:text-2xl dark:text-white">
+                  Change title
+                </h2>
+
+                <button
+                  onClick={() => {setRenaming(false);}}
+                  type="button"
+                  className="-me-4 -mt-4 rounded-full p-2 text-gray-400 transition-colors hover:bg-gray-50 hover:text-gray-600 focus:outline-none dark:text-gray-500 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+                  aria-label="Close"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+
+              <div className="mt-4">
+                <p className="text-pretty text-gray-700 dark:text-gray-200">
+                  Change the note title and click done.
+                </p>
+
+                <label htmlFor="Confirm" className="mt-4 block">
+
+                  <input
+                    type="text"
+                    id="newtitle"
+                    className="mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm dark:border-gray-600 dark:bg-gray-900 dark:text-white"
+                    defaultValue={note?.title}
+                    ref={newTitleRef}
+                  />
+                </label>
+              </div>
+
+              <footer className="mt-6 flex justify-end gap-2">
+                <button
+                  onClick={() => {setRenaming(false);}}
+                  type="button"
+                  className="rounded bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700"
+                >
+                  Cancel
+                </button>
+
+                <button
+                onClick={() => {
+                  
+                  const newTitle = newTitleRef.current?.value;
+                  if (newTitle == "" || newTitle == null || newTitle == undefined) {
+                    setError("Title of the note cannot be empty.");
+                    setTimeout(() => {setError(null);}, 2000)
+                    return;
+                  }
+                  setRenaming(false);
+                  renameNote(note, newTitle);
+                  
+                }}
+                  type="button"
+                  className="rounded bg-blue-600 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-blue-700"
+                >
+                  Done
+                </button>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </>
+      }
 
       {/* TOP BAR */}
       <div className="flex flex-wrap items-center justify-between mx-auto bg-stone-900 pl-2 pr-2 text-white h-[35px] border-white border-b">
@@ -180,19 +315,23 @@ const listNotes = notes == null ? (
           <div className="mx-1">-</div>
         </div>
         {note == null ? (
-          <div className="font-bold">FULLSTACK NOTES</div>
+          <div className="font-bold">NOTES</div>
         ) : (
-          <div className="flex">
-            <div onClick={() => {changeNote(null)}} className="mt-0 mr-1 cursor-pointer">
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 50 25">
-                <path fill="#F44336" d="M21.5 4.5H26.501V43.5H21.5z" transform="rotate(45.001 24 24)"></path><path fill="#F44336" d="M21.5 4.5H26.5V43.501H21.5z" transform="rotate(135.008 24 24)"></path>
-              </svg>
+          <div className="flex items-center">
+            <div onClick={() => {changeNote(null)}} className="cursor-pointer mr-1">
+              <Image alt="" src="svg/x.svg" width={20} height={20} />
             </div>
             <div className="font-bold">{note.title}</div>
+            <div onClick={() => {setRenaming(true)}} className="ml-2 fill-white cursor-pointer"><Image alt="" src="svg/rename.svg" width={20} height={20} /></div>
+            { uploading ? <div className="ml-2"><Image className="animate-bounce" alt="" src="svg/cloud.svg" width={22} height={22}/></div>
+                        : <div className="ml-2"><Image className="invisible" alt="" src="svg/cloud.svg" width={22} height={22}/></div>}
           </div>
         )}
         
-        <div className="">{username}</div>
+        <div className="flex items-center">
+
+          <p>{username}</p>
+        </div>
       </div>
 
       <div className={ opened ? ("h-dvh grid gap-0.5 grid-cols-[200px_1fr] bg-stone-900") : ("h-dvh grid grid-cols-[1px_1fr] gap-0.5 md:grid-cols-[200px_1fr] bg-stone-900") }>
